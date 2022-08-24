7f28bda6a2d45b68ed0ead678fce712d40bdf70f
Ilsur <yuzlikeyevi@gmail.com>
2022-08-24 11:41:58 -0500

    Added fullcard JS to show as HTML

--- js/scirpt.js
+++ js/scirpt.js
@@ -7,8 +7,10 @@ const $playerCards = $('.selection')
 function AGENTS(data) {
 
     data.map((data) => {
-        //console.log(data.displayName)
+        // console.log(data.displayName)
         let uuid = data.uuid;
+        let abilities = data.abilities;
+        console.log(abilities)
         const agentCard = `   
             <div id="${uuid}" class="indBox">
             <img src="${data.displayIcon}"/>
@@ -17,12 +19,33 @@ function AGENTS(data) {
         $playerCards.append(agentCard);
         const fullCard = `
         <div id="${data.displayName}Full" class ="fullCard">
-        <img src ="${data.fullPortrait}"/>
+        <h2>${data.displayName}</h2>
+            <img src ="${data.fullPortrait}"/>
+            <p id="description">${data.description}</p>
+            <div id="abilities">
+                <h3>Abilities</h3>
+                <div title="${abilities[0].description}" id="abilityDescription">
+                    <h4>${abilities[0].displayName}</h4>
+                    <img src ="${abilities[0].displayIcon}"/>
+                    </div>
+                <div title="${abilities[1].description} id="abilityDescription">
+                ${abilities[1].displayName}
+                    <img src ="${abilities[1].displayIcon}"/>
+                    </div>
+                <div title="${abilities[2].description} id="abilityDescription">
+                ${abilities[2].displayName}
+                    <img src ="${abilities[2].displayIcon}"/>
+                    </div>
+                <div title="${abilities[3].description} id="abilityDescription">
+                ${abilities[3].displayName}
+                    <img src ="${abilities[3].displayIcon}"/>
+                    </div>
+            </div>
         </div>
         `;
-        $(`#${data.uuid}`).on('click', function(){
+        $(`#${data.uuid}`).on('click', function () {
             console.log("works")
-           $("#fullAgent").append(fullCard)
+            $("#fullAgent").append(fullCard)
         })
     });
 
